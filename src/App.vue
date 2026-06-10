502a7b700e585a4e255589a82e77ca802334aa87
Mohsen Ashrafiamiri <ashrafimohsen@ymail.com>
2026-06-10 10:58:58 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
-<template>
-  <div class="app">
+<script setup>
+import NavBar from './components/NavBar.vue'
+import CountriesList from './components/CountriesList.vue'
+import { RouterView } from 'vue-router';
+//import { ref, onMounted } from 'vue';
 
-  </div>
-</template>
 
-<script>
+/* const countries = ref([])
+
+onMounted(async ()=> {
+  try{
+    const response = await fetch(
+      'https://ih-countries-api.herokuapp.com/countries'
+    )
+
+    countries.value = await response.json()
+  }
+  catch(error){
+    console.error('Error fetching countries:', error);
+    
+  }}
+) */
 
 </script>
 
-<style>
+<template>
+  <div class="app">
+    <NavBar/>
+    <div class="container">
+      <div class="row">
+        <div class="col-4 country-list">
+        <CountriesList/>
+        </div>
+        <div class="col-8">
+        <RouterView />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
+<style>
+.app {
+  min-height: 100vh;
+}
+.country-list {
+  height: 100vh;
+  overflow-y: auto;
+}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="navbar">
+    <h1>LAB - WikiCountries</h1>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  background-color: #f8f9fa;
+  padding: 1rem;
+}
+</style>
